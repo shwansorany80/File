0706319a195ade45509a1fbf960e614a932c81e7
shwansorany80 <91795688+shwansorany80@users.noreply.github.com>
2022-11-03 21:16:01 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("main").menu()
+       __import__("shera").menu()
    except Exception as e:
        exit(str(e))
