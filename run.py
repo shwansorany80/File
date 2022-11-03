f6f6de3e659def8ec26a5058b521cd6e964d5a6e
shwansorany80 <91795688+shwansorany80@users.noreply.github.com>
2022-11-04 01:02:10 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,23 @@
 import os
-if __name__ == "__main__":
-   try:
-       os.system("git pull")
-       __import__("shera").menu()
-   except Exception as e:
-       exit(str(e))
+
+os.system('git pull')
+
+import platform as d
+
+main = d.uname().machine
+
+if '64' in main:
+
+    import detail
+
+    detail.main()
+
+elif '32' in main:
+
+    import detail32
+
+    detail32.main()
+
+else:
+
+    exit(' Sorry Your Device Is Not Reachable !')
