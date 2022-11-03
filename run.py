d1900d5af4da34d4de0389b2f9d303a1d1fba0d9
shwansorany80 <91795688+shwansorany80@users.noreply.github.com>
2022-11-04 01:19:16 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,22 +2,5 @@ import os
 
 os.system('git pull')
 
-import platform as d
-
-main = d.uname().machine
-
-if '64' in main:
-
-    import detail
-
-    detail.main()
-
-elif '32' in main:
-
-    import detail32
-
-    detail32.main()
-
-else:
-
-    exit(' Sorry Your Device Is Not Reachable !')
+import shera
+shera.main()
